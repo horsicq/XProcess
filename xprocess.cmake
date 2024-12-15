383507139648460d76c287db777279ddfb40de06
horsicq <horsicq@gmail.com>
2024-12-16 00:42:27 +0100

    Update file(s): 2024-12-16

--- xprocess.cmake
+++ xprocess.cmake
@@ -1,8 +1,12 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
+if (NOT DEFINED XBINARY_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
+    set(XPROCESS_SOURCES ${XPROCESS_SOURCES} ${XBINARY_SOURCES})
+endif()
 
 set(XPROCESS_SOURCES
+    ${XPROCESS_SOURCES}
     ${XBINARY_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xprocess.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xprocess.h
